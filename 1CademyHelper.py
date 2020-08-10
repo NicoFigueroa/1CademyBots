8a7d58e1f76cce97f4bec0892e8c60cfdd76dba1
Nico Figueroa <nifigueroa@wccnet.edu>
2020-08-10 00:29:38 -0400

    Added initial files to begin connecting to 1cademy and handling data.

---
+++ 1CademyHelper.py
@@ -0,0 +1,18 @@
+#TODO Wrapper for 1cademy web api, need to wait for Iman to write backend code
+# to be able to write this
+#
+# These are the functions I can think of for now, we may need more as time goes on
+#  
+#
+#
+def get_nodes():
+    pass
+
+def propose_node():
+    pass
+
+def propose_reference():
+    pass
+
+def get_references():
+    pass
---
+++ ContentHelper.py
@@ -0,0 +1,20 @@
+#TODO module to vectorize and compare content between 1cademy nodes and wikipedia articles 
+# to determine if the node already exists in 1cademy
+# 
+# We may need more or different functions, this is just initial thoughts to try and 
+# find the right direction to progress in
+#  
+
+# Python module containing word2vec, https://stackabuse.com/implementing-word2vec-with-gensim-library-in-python/
+import gensim
+
+#TODO takes a wiki article summary as input and removes all numbers, 
+# changes uppercase to lower and removes references
+def sanitize_content():
+    pass
+
+def compare_vectors():
+    pass
+
+def vectorize():
+    pass

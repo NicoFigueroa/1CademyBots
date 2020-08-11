29862030aa1cea6d8a5caf3b3ec0adabac7e8118
Sohan2802 <54910728+Sohan2802@users.noreply.github.com>
2020-08-11 10:51:56 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ Please implement a module that iterates through each of the "Pages" in this arti
 Note that you need to click the nextPage button programmatically to go through multiple pages.
 
 Author: Sohan Suhas Patil
-Completed Date: 8/8/2020
+Date: 8/8/2020
 '''
 
 from selenium import webdriver
@@ -15,17 +15,19 @@ driver = webdriver.Chrome()
 driver.get("https://en.wikipedia.org/wiki/Category:Epidemiology")
 assert "Epidemiology" in driver.title
 
-css_elements = driver.find_elements_by_css_selector("div.mw-category a[href*='/wiki/']")
+css_elements = driver.find_elements_by_css_selector("#mw-pages a[href*='/wiki/']")
 
 for elem in css_elements:
     entries[elem.text] = elem.get_attribute("href")
+    print (elem.text)
 
 elem = driver.find_element_by_link_text("next page").click()
 
-css_elements = driver.find_elements_by_css_selector("div.mw-category a[href*='/wiki/']")
+css_elements = driver.find_elements_by_css_selector("#mw-pages a[href*='/wiki/']")
 
 for elem in css_elements:
     entries[elem.text] = elem.get_attribute("href")
+
     print (elem.text)
 
 assert "No results found." not in driver.page_source
